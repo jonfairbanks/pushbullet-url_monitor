756308a0eea9979bfb6bd37a51a65adb95f2bbb5
Jon Fairbanks <fairplay89@gmail.com>
2015-08-22 21:34:36 -0500

    Version 1.2
    Add Time & Log File

--- pushbullet-url_monitor.sh
+++ pushbullet-url_monitor.sh
@@ -2,19 +2,21 @@
 
 # pushbullet-url_monitor.sh - Monitors URL endpoint & sends a Pushbullet notification if unavailable
 # Author - Jon Fairbanks (https://github.com/jonfairbanks)
-# Version - 1.0
+# Version - 1.2
 
 PBTOKEN="<YOUR PUSHBULLET API TOKEN>"
 URL="<YOUR URL ENDPOINT>"
+LOG="/var/log/urlMonitor.log"
+TIME=$(date +%r)
 
-if curl --output /dev/null --silent --head --fail --connect-timeout 5 "$URL"; then
-        echo "[OK] Web Page Available on Remote Networks"
-        sleep 1
+if curl --output /dev/null --silent --head --fail --connect-timeout 10 "$URL"; then
+        echo "[OK] Web Page Available on Remote Networks" | tee -a "$LOG"
 else
         curl    -u $PBTOKEN: https://api.pushbullet.com/v2/pushes \
                 -d type=note \
                 -d title="[WARN] Web Page Not Available!" \
                 -d body="Check its status here: $URL"
-        sleep 1
+        sleep 3
+		echo "[WARN] Web Page NOT Available @ $TIME" | tee -a "$LOG"
 fi
 exit
