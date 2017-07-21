e34492eeca912830e0a69fc63400dae67ed8686a
Jon Fairbanks <jonfairbanks@paypal.com>
2017-07-21 15:56:20 -0700

    Email Monitor Pt. 1

--- email-url_monitor.sh
+++ email-url_monitor.sh
@@ -1,21 +1,18 @@
 #!/bin/bash
 
-# pushbullet-url_monitor.sh - Monitors URL endpoint & sends a Pushbullet notification if unavailable after 10 seconds.
+# email-url_monitor.sh - Monitors URL endpoint & sends an email if unavailable after 10 seconds.
 # Author - Jon Fairbanks (https://github.com/jonfairbanks)
 # Version - 1.3
 
-PBTOKEN="<YOUR PUSHBULLET API TOKEN>"
 URL="<YOUR URL ENDPOINT>"
 LOG="/var/log/pushbullet-url_monitor.log"
 TIME=$(date +%r)
+RECIPIENT="someone@somewhere.com"
 
 if curl --output /dev/null --silent --head --fail --connect-timeout 10 "$URL"; then
         echo "[OK] Web Page Available on Remote Networks" | tee -a "$LOG"
 else
-        curl    -u $PBTOKEN: https://api.pushbullet.com/v2/pushes \
-                -d type=note \
-                -d title="[WARN] Web Page Not Available!" \
-                -d body="Health check at $TIME failed."
+        mail -s "[WARN] Web Page NOT Available @ $TIME" "$RECIPIENT"
         sleep 3
 		echo "[WARN] Web Page NOT Available @ $TIME" | tee -a "$LOG"
 fi
